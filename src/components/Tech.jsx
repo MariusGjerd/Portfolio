d356f69b58ca7f78bc262437ae4fb39db8ebec38
Marius Gjerd <69842896+MariusGjerd@users.noreply.github.com>
2023-03-29 14:50:37 +0200

    updated tech section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,11 +1,14 @@
 import { BallCanvas } from "./canvas"
 import { SectionWrapper } from "../hoc"
 import { technologies } from "../constants"
+import { styles } from "../styles"
 
 
 
 const Tech = () => {
   return (
+    <>
+    <h1 className={`${styles.sectionHeadText} flex flex-row flex-wrap justify-center gap-10`}>Teach-Stack</h1>
     <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology) => (
         <div className="w-28 h-28" key={technology.name}>
@@ -13,6 +16,7 @@ const Tech = () => {
         </div>
       ))}
     </div>
+    </>
   )
 }
 
