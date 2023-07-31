ddd952f52fcd2d661fdc674df32c0318aa04e8e3
Marius Gjerd <69842896+MariusGjerd@users.noreply.github.com>
2023-07-31 13:15:46 +0200

    k

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
-import { BrowserRouter } from "react-router-dom";
-import { About, Contact, Experience, Hero, Navbar, Tech, Works } from './components';
+import { BrowserRouter } from 'react-router-dom';
+import {
+  About,
+  Contact,
+  Experience,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+} from './components';
 
 const App = () => {
   return (
     <BrowserRouter>
-    <div className="relative z-0 bg-[#092837]">
-      <div className="bg-[#092837]">
-        <Navbar/>
-        <Hero/>
+      <div className="relative z-0 bg-[#092837]">
+        <div className="bg-[#092837]">
+          <Navbar />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <div className="relative z-0">
+          <Contact />
+        </div>
       </div>
-      <About/>
-      <Experience/>
-      <Tech/>
-      <Works/>
-      <div className="relative z-0">
-        <Contact/>
-      </div>
-    </div>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
